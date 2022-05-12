d2274cd682359a1025e9fcf30ba7382069962863
lisisiejid <1551505032@qq.com>
2022-05-12 21:24:09 +0800

    完善apache漏洞分析，包括sql注入和xss注入，完善告警，流量分析摆烂中...

--- app.py
+++ app.py
@@ -3,14 +3,13 @@ from threading import Thread
 
 from flask import Flask, request, render_template, redirect, url_for, session, flash
 from flask_sqlalchemy import SQLAlchemy
-from admin import user,ipanalyse,sshanalyse,networkanalyse,processanalyse,apacheanalyse
-from charts import apacheCharts,earthMapCharts,sshCharts,networkCharts,processCharts,attackeventCharts
-from charts.manage import ip_manageCharts, dangerous_manageCharts,event_manageCharts
-from admin.manage import ip_manage,event_manage
-from flask_mail import Mail,Message
+from admin import user, ipanalyse, sshanalyse, networkanalyse, processanalyse, apacheanalyse
+from charts import apacheCharts, earthMapCharts, sshCharts, networkCharts, processCharts, attackeventCharts
+from charts.manage import ip_manageCharts, dangerous_manageCharts, event_manageCharts
+from admin.manage import ip_manage, event_manage
+from flask_mail import Mail, Message
 from admin.manage import dangerous_manage
 from admin.manage import defend as Defend
-
 
 # 建立flask对象
 app = Flask(__name__)
@@ -27,30 +26,32 @@ db = SQLAlchemy(app)
 ncap = Thread(target=networkanalyse.networkanalyse)
 ncap.start()
 
-check=Thread(target=dangerous_manage.danger)
+check = Thread(target=dangerous_manage.danger)
 check.start()
 
+
 # 初始化
-@app.route('/',methods=['GET','POST'])
+@app.route('/', methods=['GET', 'POST'])
 def init():
-    if session.get('username')!='admin':
+    if session.get('username') != 'admin':
         return render_template('login.html')
     else:
         # sshanalyse.analyseByfile()
         # apacheanalyse.apacheanalyse()
         return redirect(url_for('index'))
 
+
 # 登录面板
-@app.route('/login',methods=['GET','POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        username= request.form['username']
-        password= request.form['password']
-        username=Defend.waffilter(username)#过滤处理
-        password=Defend.waffilter(password)
+        username = request.form['username']
+        password = request.form['password']
+        username = Defend.waffilter(username)  # 过滤处理
+        password = Defend.waffilter(password)
 
-        info=user.check(str(username),str(password))
-        if info=='登录成功':
+        info = user.check(str(username), str(password))
+        if info == '登录成功':
             session['username'] = 'admin'
             return redirect(url_for('index'))
         else:
@@ -58,36 +59,35 @@ def login():
     else:
         return render_template('login.html')
 
+
 # 密码修改
-@app.route('/admin_info',methods=['GET','POST'])
+@app.route('/admin_info', methods=['GET', 'POST'])
 def admin_info():
-    if session.get('username')!='admin':
+    if session.get('username') != 'admin':
         return render_template('login.html')
     if request.method == 'POST':
-        username= request.form['username']
-        password= request.form['password']
-        newpassword=request.form['newpassword']
-        renewpassword=request.form['renewpassword']
+        username = request.form['username']
+        password = request.form['password']
+        newpassword = request.form['newpassword']
+        renewpassword = request.form['renewpassword']
 
-        username=Defend.waffilter(username)#过滤处理
-        password=Defend.waffilter(password)
-        newpassword=Defend.waffilter(newpassword)
-        renewpassword=Defend.waffilter(renewpassword)
+        username = Defend.waffilter(username)  # 过滤处理
+        password = Defend.waffilter(password)
+        newpassword = Defend.waffilter(newpassword)
+        renewpassword = Defend.waffilter(renewpassword)
 
-        if newpassword!=renewpassword:
-            info="两次密码错误"
-            return render_template('manage/admin_info.html',info=info)
-        info=user.updatepassword(str(username),str(password),str(newpassword))
-        if info:#修改失败
-            info="密码错误"
-            return render_template('manage/admin_info.html',info=info,)
+        if newpassword != renewpassword:
+            info = "两次密码错误"
+            return render_template('manage/admin_info.html', info=info)
+        info = user.updatepassword(str(username), str(password), str(newpassword))
+        if info:  # 修改失败
+            info = "密码错误"
+            return render_template('manage/admin_info.html', info=info, )
         else:
-            info="修改成功"
-            return render_template('manage/admin_info.html',info=info)
+            info = "修改成功"
+            return render_template('manage/admin_info.html', info=info)
     else:
         return render_template('manage/admin_info.html')
-
-
 
 
 # #防火墙
@@ -97,29 +97,29 @@ def admin_info():
 #     dangerous_manageCharts.dealdangerbyself(ip)
 
 
-#ip管理
-@app.route('/ip_manage',methods=['GET','POST'])
+# ip管理
+@app.route('/ip_manage', methods=['GET', 'POST'])
 def ip_manage():
-    if session.get('username')!='admin':
+    if session.get('username') != 'admin':
         return render_template('login.html')
     if request.method == 'POST':
         ip = request.form['defend_ip']
-        ip=Defend.waffilter(ip) #过滤处理
-        result=dangerous_manageCharts.dealdangerbyself(ip)
-        if result==0:
-            print(ip+" 防御失败")
+        ip = Defend.waffilter(ip)  # 过滤处理
+        result = dangerous_manageCharts.dealdangerbyself(ip)
+        if result == 0:
+            print(ip + " 防御失败")
 
-    defend_index_list=[]
-    length,ip_list,country_name,country_specificname,city_name,time=ip_manageCharts.selectalllistCharts()
+    defend_index_list = []
+    length, ip_list, country_name, country_specificname, city_name, time = ip_manageCharts.selectalllistCharts()
     for i in ip_list:
         defend_index = dangerous_manageCharts.selectisdeal(i)
         defend_index_list.append(defend_index)
         # info_list.append("已防御")
     # print(defend_index_list)
-    ipcountry_pie,foreign_num = ip_manageCharts.selectby_countryCharts()
-    ipcity_pie,china_num=ip_manageCharts.selectby_chinacityCharts()
+    ipcountry_pie, foreign_num = ip_manageCharts.selectby_countryCharts()
+    ipcity_pie, china_num = ip_manageCharts.selectby_chinacityCharts()
     earthmapcharts = earthMapCharts.earthMap()
-    dangerBar=dangerous_manageCharts.dangerBarCharts()
+    dangerBar = dangerous_manageCharts.dangerBarCharts()
 
     return render_template('manage/ipmanage.html',
                            ipcountry_pie=ipcountry_pie.render_embed(),
@@ -137,8 +137,11 @@ def ip_manage():
                            dangerBar=dangerBar.render_embed(),
                            )
 
+
 # 发送邮箱，用于告警
 mail = Mail(app)
+
+
 # @app.route('/sendEmail')
 # def sendEmail():
 #     msg = Message(subject='服务器遭受攻击',sender='1551505032@qq.com',recipients=['2534395766@qq.com'])
@@ -154,17 +157,17 @@ mail = Mail(app)
 #     # return render_template("test.html")
 
 
-#原始数据
-@app.route('/get_raw_data',methods=['GET','POST'])
+# 原始数据
+@app.route('/get_raw_data', methods=['GET', 'POST'])
 def get_raw_data():
-    if session.get('username')!='admin':
+    if session.get('username') != 'admin':
         return render_template('login.html')
-    ip=request.args.get('ip')
+    ip = request.args.get('ip')
     ip = Defend.waffilter(ip)  # 过滤处理
-    event_num=event_manageCharts.dealevent_numCharts(ip)
-    length=len(event_num)
-    apache_raw,ssh_raw,network_raw=event_manage.getraw_data(ip)
-    print(apache_raw,ssh_raw,network_raw)
+    event_num = event_manageCharts.dealevent_numCharts(ip)
+    length = len(event_num)
+    apache_raw, ssh_raw, network_raw = event_manage.getraw_data(ip)
+    print(apache_raw, ssh_raw, network_raw)
     return render_template("manage/raw_data.html",
                            event_num=event_num,
                            length=length,
@@ -173,6 +176,7 @@ def get_raw_data():
                            network_raw=network_raw,
                            ip=ip,
                            )
+
 
 # @app.route('/danger',methods=['GET','POST'])
 # def danger():
@@ -202,33 +206,33 @@ def get_raw_data():
 # def config():
 #     return 'dd'
 
-#面板首页
-@app.route('/index',methods=['GET','POST'])
+# 面板首页
+@app.route('/index', methods=['GET', 'POST'])
 def index():
-    if session.get('username')!='admin':
+    if session.get('username') != 'admin':
         return render_template('login.html')
-    #图表绘制
-    #sshanalyse.analyseByfile()
-    #apacheanalyse.apacheanalyse()
-    apachecharts=apacheCharts.apachePieCharts()
+    # 图表绘制
+    # sshanalyse.analyseByfile()
+    # apacheanalyse.apacheanalyse()
+    apachecharts = apacheCharts.apachePieCharts()
     apache_id = apachecharts._chart_id
-    earthmapcharts=earthMapCharts.earthMap()
-    earthmap_id=earthmapcharts._chart_id
-    sshcharts,risk_index=sshCharts.sshPieCharts()
-    sshcharts_id=sshcharts._chart_id
-    networkcharts=networkCharts.networkcharts()#饼状图
-    networkcharts_id=networkcharts.chart_id
-    streamcharts=networkCharts.streamcharts()
-    processcharts=processCharts.processCharts()
-    ip_list,time_list,type_list=attackeventCharts.selectevent()
-    flag=0
+    earthmapcharts = earthMapCharts.earthMap()
+    earthmap_id = earthmapcharts._chart_id
+    sshcharts, risk_index = sshCharts.sshPieCharts()
+    sshcharts_id = sshcharts._chart_id
+    networkcharts = networkCharts.networkcharts()  # 饼状图
+    networkcharts_id = networkcharts.chart_id
+    streamcharts = networkCharts.streamcharts()
+    processcharts = processCharts.processCharts()
+    ip_list, time_list, type_list = attackeventCharts.selectevent()
+    flag = 0
     for i in type_list:
         # print(i)
-        if i=='木马后门' or i=='SQL注入':
-            flag=1
+        if i == '木马后门' or i == 'SQL注入':
+            flag = 1
         else:
             pass
-    if flag==1:
+    if flag == 1:
         msg = Message(subject='服务器遭受攻击', sender='yoursender@qq.com', recipients=['2534395766@qq.com'])
         msg.body = '您的服务器正遭受攻击，请前往态势感知系统查看！！'
         msg.html = '<b>您的服务器正遭受攻击，请前往态势感知系统查看！！</b> '
@@ -236,7 +240,7 @@ def index():
     else:
         pass
 
-    length=len(ip_list)
+    length = len(ip_list)
     return render_template('base.html',
                            apachecharts=apachecharts.render_embed(),
                            apache_id=apache_id,
@@ -258,4 +262,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=5000,debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
