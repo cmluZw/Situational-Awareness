7bb0e3de71bba86a635e83e01e46e2c776805ef6
cmluZw <78641812+cmluZw@users.noreply.github.com>
2023-04-27 22:10:34 +0800

    Delete charts directory

--- charts/manage/event_manageCharts.py
+++
@@ -1,9 +0,0 @@
-from admin.manage import event_manage
-from pyecharts import Pie
-
-def dealevent_numCharts(ip):
-    event_num=event_manage.selectevent_byip(ip)
-    return event_num
-
-# dealevent_numCharts("101.33.228.166")
-
