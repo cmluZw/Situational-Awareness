102a38ab6fbfee84c5f01e35454b8de75cb0f167
cmluZw <78641812+cmluZw@users.noreply.github.com>
2023-04-27 22:10:24 +0800

    Delete admin directory

--- admin/manage/autodefend.py
+++
@@ -1,7 +0,0 @@
-import time
-from charts.manage import dangerous_manageCharts
-from charts.manage.dangerous_manageCharts import dealdanger
-
-while 1:
-    dealdanger()
-    time.sleep(7200)#每两个小时执行一次
